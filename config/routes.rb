3940b3695791d1df79808bb834013f0415dd03e3
Charity Fuller <charryef@gmail.com>
2018-08-29 08:48:18 -0700

    Checkpoint Rails Topics and Posts: completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :posts
-  
+  resources :topics do
+    resources :posts, except: [:index]
+  end
+
   get 'about' => 'welcome#about'
 
   root 'welcome#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
