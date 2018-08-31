6abefa54ddc55e02ac0c424563e9800053cb6b93
Charity Fuller <charryef@gmail.com>
2018-08-30 20:01:26 -0700

    Assignment Rais Topics & Posts: completed

--- db/migrate/20180830091019_add_topic_to_sponsored_post.rb
+++
@@ -1,6 +0,0 @@
-class AddTopicToSponsoredPost < ActiveRecord::Migration[5.2]
-  def change
-    add_column :sponsored_posts, :topic_id, :integer
-    add_index :sponsored_posts, :topic_id
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,13 +11,24 @@ topics = Topic.all
 
 # Create Posts
 50.times do
-  Post.create
+  Post.create(
     topic:  topics.sample,
     title:  RandomData.random_sentence,
     body:   RandomData.random_paragraph
   )
 end
 posts = Post.all
+
+# Create SponsoredPost
+50.times do
+  SponsoredPost.create(
+    topic:  topics.sample,
+    title:  RandomData.random_sentence,
+    body:   RandomData.random_paragraph,
+    price:  rand(100)
+  )
+end
+sponsored_posts = SponsoredPost.all
 
 #Create Comments
 100.times do
@@ -31,3 +42,4 @@ puts "Seed finished"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{SponsoredPost.count} sponsored_posts created"
