de28438f8e768b1ee6fb2624e50d44a459d3f264
Charity Fuller <charryef@gmail.com>
2018-09-04 14:24:48 -0700

    Checkpoint Rails Posts and Users: completed

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,5 @@
-class Question < ApplicationRecord
-  has_many :answers
-end
+# Comment out as this wasn't supposed to be merged from the previous assignment
+
+# class Question < ApplicationRecord
+#   has_many :answers
+# end
