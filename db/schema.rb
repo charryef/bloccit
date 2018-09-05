77b363c029b860b7cee3baa577c0a623516e9f13
Charity Fuller <charryef@gmail.com>
2018-09-05 12:23:09 -0700

    Updated bootsnap

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_31_213002) do
+ActiveRecord::Schema.define(version: 2018_09_05_190630) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2018_08_31_213002) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role"
   end
 
 end
