0b05fc7ea96d99ab717f6e91989fddba2878e87f
Charity Fuller <charryef@gmail.com>
2018-09-21 00:10:36 -0700

    Assignment Rails Authorization: completed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -47,19 +47,6 @@ RSpec.describe User, type: :model do
        expect(user.role).to eql("member")
      end
 
-     context "moderator user" do
-       before do
-         user.moderator!
-       end
-       it "returns false for #member" do
-         expect(user.member?).to be_falsey
-       end
-
-       it "returns true for #moderator?" do
-         expect(user.moderator?).to be_truthy
-       end
-     end
-
      context "member user" do
        it "returns true for #member?" do
          expect(user.member?).to be_truthy
@@ -67,6 +54,10 @@ RSpec.describe User, type: :model do
 
        it "returns false for #admin?" do
          expect(user.admin?).to be_falsey
+       end
+
+       it "returns false for #moderator?" do
+         expect(user.moderator?).to be_falsey
        end
      end
 
@@ -82,7 +73,30 @@ RSpec.describe User, type: :model do
        it "returns true for #admin?" do
          expect(user.admin?).to be_truthy
        end
+
+       it "returns false for #moderator?" do
+         expect(user.moderator?).to be_falsey
+       end
      end
+
+     context "moderator user" do
+       before do
+         user.moderator!
+       end
+
+       it "returns false for #member?" do
+         expect(user.member?).to be_falsey
+       end
+
+       it "returns false for #admin?" do
+         expect(user.admin?).to be_falsey
+       end
+
+       it "returns true for #moderator?" do
+         expect(user.moderator?).to be_truthy
+       end
+     end
+
     end
 
   describe "invalid user" do
