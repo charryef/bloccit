30fc86a8d1b094807b42d301a4742e207bfbe666
Charity Fuller <charryef@gmail.com>
2018-09-20 22:46:03 -0700

    Initial commit for Checkpoint Rails Voting

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_07_161037) do
+ActiveRecord::Schema.define(version: 2018_09_21_042521) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -49,6 +49,16 @@ ActiveRecord::Schema.define(version: 2018_09_07_161037) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role"
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer "value"
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_votes_on_post_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
   end
 
 end
